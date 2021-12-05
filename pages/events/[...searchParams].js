875055dcb0563801c93a7e95568e4fecc31c6b90
Kayode Odole <odoleemma@gmail.com>
2021-12-05 14:32:34 +0100

    update catch all route for events

--- pages/events/[...searchParams].js
+++ pages/events/[...searchParams].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../DUMMY_DATA";
 import EventList from "../../components/events/eventList";
+import ResultsTitle from "../../components/events/results-title";
 
 function FilteredEventPage() {
   const { searchParams } = useRouter().query;
@@ -12,11 +13,19 @@ function FilteredEventPage() {
       </Container>
     );
   }
-  const [year, month] = searchParams;
-  const events = getFilteredEvents({ year: +year, month: +month });
-
+  const [year, month] = [+searchParams[0], +searchParams[1]];
+  if (isNaN(year) || isNaN(month) || month > 12) {
+    return (
+      <Container>
+        <p>Invalid parameter please adjust and try again!</p>
+      </Container>
+    );
+  }
+  const events = getFilteredEvents({ year, month });
+  const date = new Date(year, month - 1);
   return (
     <Container>
+      <ResultsTitle date={date} />
       {events.length > 0 ? (
         <EventList events={events} />
       ) : (
@@ -28,10 +37,5 @@ function FilteredEventPage() {
 export default FilteredEventPage;
 
 function Container({ children }) {
-  return (
-    <div style={{ marginLeft: "1rem" }}>
-      <h1>Filtered Page</h1>
-      {children}
-    </div>
-  );
+  return <div style={{ marginLeft: "1rem" }}>{children}</div>;
 }
